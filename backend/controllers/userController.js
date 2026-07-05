f297ab05b5cc01e99c629aa6d41f9427b0bceed8
developermickey <mukesh.quazma@gamil.com>
2026-07-06 02:21:17 +0530

    User route secure with good way

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -42,3 +42,47 @@ export const userRegister = async (req, res) => {
     });
   }
 };
+
+export const userLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found, please register",
+      });
+    }
+
+    const mathcPassword = await bcrypt.compare(password, user.password);
+    if (!mathcPassword) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "User Loggedin successfully",
+      data: {
+        name: user.fullname,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error("Register Route Server Error", error.message);
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
