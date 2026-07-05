8e3383b0ade213d104d1cff01726485ef8136ef9
developermickey <mukesh.quazma@gamil.com>
2026-07-06 02:04:53 +0530

    User Data Created Successfully also add Json Data

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import express from "express";
 import connectDB from "./config/db.js";
 const app = express();
 
+app.use(express.json());
+
 import userRoutes from "./routes/userRoutes.js";
 
 const PORT = process.env.PORT || 5001;
