db4a7181498fe7a27056b519892f8bcea56aee02
developermickey <mukesh.quazma@gamil.com>
2026-07-06 01:50:33 +0530

    user Controller added

---
+++ backend/controllers/userController.js
@@ -0,0 +1,41 @@
+import express from "express";
+import User from "../models/User.js";
+
+export const userRegister = async (req, res) => {
+  try {
+    const { fullname, email, password } = req.body;
+    if (!fullname || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All feild are requried",
+      });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(409).json({
+        success: false,
+        message: "User already exists, please login",
+      });
+    }
+
+    const newUser = await User.create({
+      fullname,
+      email,
+      password,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "User register successfully",
+      newUser,
+    });
+  } catch (error) {
+    console.error("Register Route Server Error", error.message);
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
